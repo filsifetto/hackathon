1c17ea3715754d55cea13d161d331aaa08877a0d
filsifetto <axelfi@stud.ntnu.no>
2026-02-13 17:51:50 +0100

    fiewj

---
+++ src/avatar/backend/modules/lip-sync.mjs
@@ -0,0 +1,55 @@
+import { convertTextToSpeech } from "./elevenLabs.mjs";
+import { getPhonemes } from "./rhubarbLipSync.mjs";
+import { readJsonTranscript, audioFileToBase64 } from "../utils/files.mjs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const backendDir = path.resolve(__dirname, "..");
+
+const MAX_RETRIES = 10;
+const RETRY_DELAY = 0;
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const lipSync = async ({ messages }) => {
+  await Promise.all(
+    messages.map(async (message, index) => {
+      const fileName = `audios/message_${index}.mp3`;
+
+      for (let attempt = 0; attempt < MAX_RETRIES; attempt++) {
+        try {
+          await convertTextToSpeech({ text: message.text, fileName });
+          await delay(RETRY_DELAY);
+          break;
+        } catch (error) {
+          if (error.response && error.response.status === 429 && attempt < MAX_RETRIES - 1) {
+            await delay(RETRY_DELAY);
+          } else {
+            throw error;
+          }
+        }
+      }
+      console.log(`Message ${index} converted to speech`);
+    })
+  );
+
+  await Promise.all(
+    messages.map(async (message, index) => {
+      const mp3Path = path.join(backendDir, "audios", `message_${index}.mp3`);
+      const jsonPath = path.join(backendDir, "audios", `message_${index}.json`);
+
+      try {
+        await getPhonemes({ message: index });
+        message.audio = await audioFileToBase64({ fileName: mp3Path });
+        message.lipsync = await readJsonTranscript({ fileName: jsonPath });
+      } catch (error) {
+        console.error(`Error while getting phonemes for message ${index}:`, error);
+      }
+    })
+  );
+
+  return messages;
+};
+
+export { lipSync };
